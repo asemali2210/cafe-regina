509a5bb7067dc989b4f158b01c4fa81861ee1c64
asemali <asemali2210@gmail.com>
2025-09-01 23:47:21 +0300

    add about header

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,19 @@
+import ArrowLink from "@/components/LinkArrow/ArrowLink";
 import Navbar from "@/components/navbar/Navbar";
 
 export default function About() {
   return (
     <div  className="abot__page">
       <Navbar />
+      <header>
+        <p className="h2">Know about cafe </p>
+        <div className="header__bottom d-flex">
+            <p className="h2">Regina</p>
+            <p>Are you curious about the oldest café in Zelzate ? Then look no further, because Café Regina is the place to be. This is not only the oldest, but also the nicest café in Zelzate and the surrounding area. So be sure to come by and enjoy a good time! </p>
+        
+        </div>
+        <ArrowLink href="contact" content="CONTACT US"/>
+      </header>
     </div>
   );
 }

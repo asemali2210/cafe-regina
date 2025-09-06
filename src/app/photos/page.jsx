c67559235aedc2306720b7a50f0054893999ac03
asemali <asemali2210@gmail.com>
2025-09-06 23:24:34 +0300

    add Photos page with PageHeader, HomeGallery and Newsletter

---
+++ src/app/photos/page.jsx
@@ -0,0 +1,27 @@
+import DividerLogo from "@/components/DividerLogo/DividerLogo";
+import Newsletter from "@/components/shared/Newsletter/Newsletter";
+import PageHeader from "@/components/shared/PageHeader/PageHeader";
+import React from "react";
+import HomeGallery from "@/components/home/HomeGallery/HomeGallery";
+
+function PhotosPage() {
+  return (
+    <div className="bg-dark-1">
+      <PageHeader
+        title1="Enjoy The Atmospheric"
+        title2="Photos!"
+        descirptions={
+          "If you are looking for an authentic café in Zelzate, Café Regina is the right place for you! You can be inspired by the atmospheric photos on this page. This gives you a taste of what you can expect when you visit the café. So be sure to check out these photos!"
+        }
+        linkContent="contact"
+        linkHref="/contact"
+      />
+
+      <HomeGallery />
+      <Newsletter />
+    </div>
+  );
+}
+
+export default PhotosPage;
+

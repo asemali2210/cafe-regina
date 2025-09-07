2cfd424e3494b0d1bb2ada7a99f49b25b1120ed3
asemali <asemali2210@gmail.com>
2025-09-07 03:20:39 +0300

    events: add /events page with navbar, back-to-home link, left-image/right-details layout; styles in events.scss

---
+++ src/app/events/page.jsx
@@ -0,0 +1,43 @@
+import Navbar from "@/components/navbar/Navbar";
+import ArrowLink from "@/components/LinkArrow/ArrowLink";
+import { events } from "@/data/siteData";
+import Image from "next/image";
+import React from "react";
+import "@/style/pages/events.scss";
+
+function EventsPage() {
+  return (
+    <div className="events-page bg-dark-1">
+      <Navbar />
+      <div className="container">
+        <div className="back-link">
+          <ArrowLink href="/" content="Back To Home" />
+        </div>
+
+        <div className="events-list">
+          {events.map((ev) => (
+            <div key={ev.id} className="event-row row">
+              <div className="col-12 col-lg-5 event-row__image">
+                <Image
+                  src={ev.imgSrc}
+                  alt={ev.title}
+                  className="img-fluid"
+                  width={600}
+                  height={400}
+                />
+              </div>
+              <div className="col-12 col-lg-7 d-flex flex-column justify-content-center">
+                <p className="event-row__date font-inter">{ev.date}</p>
+                <h3 className="event-row__title font-athina">{ev.title}</h3>
+                <p className="event-row__desc font-inter">{ev.description}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default EventsPage;
+

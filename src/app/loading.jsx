8d4e94224653245532714f618a0b67a731f5d5e2
asemali <asemali2210@gmail.com>
2025-09-27 22:49:43 +0300

    remove loading

--- src/app/loading.jsx
+++
@@ -1,9 +0,0 @@
-﻿import LogoLoader from "../components/shared/Loader/LogoLoader";
-
-export default function Loading() {
-  return (
-    <div className="fixed inset-0 z-[9999] flex items-center justify-center bg-black/90 backdrop-blur-sm">
-      <LogoLoader />
-    </div>
-  );
-}
